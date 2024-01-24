29a9b5a59231eff92a31cad063467cf1cb3ad7fb
SabatinoFossemo <sabatinofossemo@gmail.com>
2024-01-24 10:45:24 +0100

    author

--- etoroscraper/login.py
+++ etoroscraper/login.py
@@ -4,10 +4,5 @@ import time
 
 def login(driver, login_page):
     driver.get(login_page)
-    user = driver.find_element(By.XPATH, '//*[@id="username"]')
-    password = driver.find_element(By.XPATH, '//*[@id="password"]')
-    time.sleep(2)
-    user.send_keys('ksher83')
-    time.sleep(2)
-    password.send_keys('UnderR00m1')
-    input('PRESS ENTER TO CONTINUE')
+
+
--- etoroscraper/main.py
+++ etoroscraper/main.py
@@ -13,31 +13,55 @@ pd.set_option('display.max_columns', None)
 
 if __name__ == '__main__':
 
-    print(Config.BASE)
-    print(Config.MARKETS)
-    print(Config.EXCHANGES)
-
     # Start Chrome Driver
     options = uc.ChromeOptions()
-    options.headless = False  # Set headless to False to run in non-headless mode
+    # Set headless to False to run in non-headless mode
+    options.headless = False
 
-    # Login
+    # Login page
     driver = uc.Chrome(use_subprocess=True, options=options)
     login(driver, Config.LOGIN)
-
+    input('PRESS ENTER AFTER LOGGING IN')
     # SCRAPE STOCKS
-
     for exchange in Config.EXCHANGES:
         print(exchange)
+
         driver.get(exchange)
+
         input('Exchange Loaded?')
         time.sleep(5)
-        xp_rows = '//*[@automation-id="discover-market-results-row"]'
-        rows = driver.find_elements(By.XPATH, xp_rows)
-        for row in rows:
-            xp_tick = '//*[@automation-id="trade-item-name"]'
-            xp_name = '//*[@automation-id="trade-item-full-name"]'
-            ticker = row.find_element(By.XPATH, xp_tick).text
-            name = row.find_element(By.XPATH, xp_name).text
-            print(f'{ticker}, {name}')
+
+        num_items = driver.find_element(By.XPATH, '//*[@automation-id="discover-market-results-num"]')
+        num_items = int(num_items.text.replace(',', ''))
+        print(f'{num_items} items found')
+
+        next_button = driver.find_element(By.XPATH, '//*[@automation-id="discover-market-next-button"]')
+
+        stocks = []
+        while True:
+            counted = 0
+            xp_rows = '//*[@automation-id="trade-item-info"]'
+            rows = driver.find_elements(By.XPATH, xp_rows)
+
+            for row in rows:
+                xp_tick = './/*[@automation-id="trade-item-name"]'
+                xp_name = './/*[@automation-id="trade-item-full-name"]'
+                ticker = row.find_element(By.XPATH, xp_tick).text
+                name = row.find_element(By.XPATH, xp_name).text
+                stocks.append((ticker, name))
+                print(f'{ticker}, {name}')
+                counted += 1
+
+            num_items -= counted
+            print(f'{counted} counted items in this page\n'
+                  f'{num_items} items remaining\n'
+                  f'{len(stocks)} item collected')
+
+            if num_items <= 0:
+                break
+            else:
+                next_button.click()
+            input('PRESS ENTER WHEN PAGE IS LOADED')
+            time.sleep(2)
+
     driver.close()
